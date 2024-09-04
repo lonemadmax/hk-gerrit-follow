lonemadmax/hk-gerrit-follow

commit 9da7a7e487f9794eb22ce622778fee9c07fdff0a

Web: show build results in main table only for current version
Despite the colour change, it can be confusing to see a build result for a previous version in the main table.

web/js/summary.js
@@ -398,24 +398,28 @@
             const expandCell = text('td', change.queue ?? '');
             tr.appendChild(expandCell);
             let age = 'age1';
+            let current = false;
             if (change.build.length > 0) {
                 if (change.build.length > 1) {
                     expandCell.appendChild(changesetBuildsTable(change));
                 }
                 const lastbuild = change.build[0];
-                tr.appendChild(text('td', app.util.timeString(lastbuild.time)));
-                const parent = lastbuild.parent;
-                const parentCell = compose('td',
-                    textLink(releaseBasePath(parent.tag), parent.tag));
-                    //textLink('#'+parent.tag, parent.tag));
-                parentCell.classList.add('age'+parent.age);
-                tr.appendChild(parentCell);
-                tr.appendChild(compose('td',
-                    changesetStateFragment(k, lastbuild)));
                 if (lastbuild.version == change.version) {
                     age = 'age0';
+                    current = true;
+                    tr.appendChild(text('td',
+                        app.util.timeString(lastbuild.time)));
+                    const parent = lastbuild.parent;
+                    const parentCell = compose('td',
+                        textLink(releaseBasePath(parent.tag), parent.tag));
+                        //textLink('#'+parent.tag, parent.tag));
+                    parentCell.classList.add('age'+parent.age);
+                    tr.appendChild(parentCell);
+                    tr.appendChild(compose('td',
+                        changesetStateFragment(k, lastbuild)));
                 }
-            } else {
+            }
+            if (!current) {
                 tr.appendChild(text('td', ''));
                 tr.appendChild(text('td', ''));
                 tr.appendChild(text('td', ''));

commit 801e74de475fa2c83d3e0de9e31d54924749c68d

Web: show result from previous build when it is not the last version
9da7a7e487f9794eb22ce622778fee9c07fdff0a broke the case when there's
just one build and a new one is required.

web/js/summary.js
@@ -369,9 +369,6 @@
             let age = 'age1';
             let current = false;
             if (change.build.length > 0) {
-                if (change.build.length > 1) {
-                    expandCell.appendChild(changesetBuildsTable(change));
-                }
                 const lastbuild = change.build[0];
                 if (lastbuild.version == change.version) {
                     age = 'age0';
@@ -386,6 +383,9 @@
                     tr.appendChild(parentCell);
                     tr.appendChild(compose('td',
                         changesetStateFragment(k, lastbuild)));
+                }
+                if (change.build.length > 1 || !current) {
+                    expandCell.appendChild(changesetBuildsTable(change));
                 }
             }
             if (!current) {